80744d8ea896bf04660cffd4226706d069f2cb12
Syed-Bilawal <shahb7599@.com>
2023-07-24 17:43:40 +0500

    read me

--- lib/image_screen.dart
+++ lib/image_screen.dart
@@ -1,47 +1,8 @@
-// import 'package:flutter/material.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter/material.dart';
-// import 'package:carousel_slider/carousel_slider.dart';
-// import 'package:my_test_app/image_screen.dart';
-
-// import 'models/product.dart';
-// class ImageScreen extends StatefulWidget {
-//   final String url;
-//   ImageScreen(this.url);
-
-//   @override
-//   _MyImageScreen createState() => _MyImageScreen(url);
-// }
-
-// class _MyImageScreen extends State<ImageScreen> {
-//   // final String url;
-//   // _MyImageScreen(this.url);
-//     final productItems store;
-//   _MyImageScreen(this.store);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//         appBar: AppBar(
-//           title: Text('ImageScreen'),
-//         ),
-        
-//         body: Column( 
-//           children: [ Align(alignment: Alignment.center,),
-//             Image.network(store.urlImage, width: 500 ,),
-//           ],
-//         ));
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 
 import 'models/product.dart';
-
-
-
-
 class TheDescriptionPage extends StatelessWidget {
   final productItems store;
   TheDescriptionPage(this.store);
@@ -81,15 +42,20 @@ class TheDescriptionPage extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [ 
                     Center(
-                      
                       child: Align(
                         alignment: Alignment.topLeft,
                         child: Text( 
                           store.name,
                           style: TextStyle(fontSize: 35,),
                         ),
+                      
                       ),
+                      
                     ),
+                    Container(
+                      child:
+                      Text(store.price),
+                         ),
                     Text(
                       'Description:',
                       style: TextStyle(

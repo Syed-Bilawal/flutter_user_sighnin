df19ab243714560fa5a2d50d591dc8fcd8a25bc2
Syed-Bilawal <shahb7599@.com>
2023-07-26 12:47:04 +0500

    read me

--- lib/controllers/user_controller.dart
+++ lib/controllers/user_controller.dart
@@ -4,7 +4,9 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:get/route_manager.dart';
+import 'package:my_test_app/models/user.dart';
 import 'package:my_test_app/views/screens/home_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class UserController extends GetxController {
   TextEditingController emailC = TextEditingController();
@@ -12,13 +14,16 @@ class UserController extends GetxController {
   TextEditingController user= TextEditingController();
   final formKey = GlobalKey<FormState>();
    String? str;
-  void signIn() {
+  void signIn() async{
     
     if (formKey.currentState!.validate()) {
     if (str == null) {
     }
     print("Valid");
     Get.snackbar('Success', 'User Logged In Successfully');
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    Sinup user = Sinup('asdd', emailC.text, passC.text);
+    await prefs.setString('user', user.toJson());
     Get.to(
         transition: Transition.fadeIn,
         duration: const Duration(seconds: 1),
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
 import 'package:my_test_app/views/screens/home_screen.dart';
 import 'package:my_test_app/views/screens/signin_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'models/user.dart';
 
 void main() {
   runApp(const MyApp());
 }
+  Sinup? user;
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -30,8 +35,44 @@ class MyApp extends StatelessWidget {
           ),
         ),
       ),
+      home  : SplashScreen(),
       // home: SignInScreen(),
-      home: HomeScreen(email: 'email'),
+      //home: HomeScreen(email: 'email'),
     );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    myInit();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+  
+  void myInit() async{
+      
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  if (prefs.containsKey('user')){
+    String userJson = prefs.getString('user')!;
+    print('user-> $userJson');
+    user = Sinup.fromJson(userJson); 
+    Get.to(() => HomeScreen(email: 'asdasd'));
+  }
+  else{
+    Get.to(() => SignInScreen());
+  }
+
+
   }
 }
